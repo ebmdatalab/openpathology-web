7ed44ff41fa58aaea7f4ed400ec77596aadea3e8
Seb Bacon <seb.bacon@gmail.com>
2019-10-21 10:15:53 +0100

    Move datatable generation to "base" app

--- dashapp/apps/base.py
+++ dashapp/apps/base.py
@@ -4,6 +4,7 @@ from dash.dependencies import Input, Output
 
 from app import app
 from stateful_routing import get_state
+from data import get_count_data
 import settings
 
 
@@ -38,3 +39,38 @@ def get_sorted_group_keys(df, group_by):
         axis=0,
     ).index
     return entity_ids
+
+
+@app.callback(Output("counts-table", "data"), [Input("page-state", "children")])
+def update_counts_table(page_state):
+    page_state = get_state(page_state)
+    if page_state.get("page_id") != settings.COUNTS_CHART_ID:
+        return []
+
+    numerators = page_state.get("numerators", [])
+    denominators = page_state.get("denominators", [])
+    result_filter = page_state.get("result_filter", [])
+    groupby = page_state.get("groupby", None)
+    practice_filter_entity = page_state.get("practice_filter_entity", None)
+    entity_ids_for_practice_filter = page_state.get(
+        "entity_ids_for_practice_filter", []
+    )
+
+    # XXX should page with python here
+    # See https://dash.plot.ly/datatable/callbacks
+    df = get_count_data(
+        numerators=numerators,
+        denominators=denominators,
+        by=None,
+        practice_filter_entity=practice_filter_entity,
+        entity_ids_for_practice_filter=entity_ids_for_practice_filter,
+        result_filter=result_filter,
+    )
+    # XXX make downloadable:
+    # https://github.com/plotly/dash-core-components/issues/216 -
+    # perhaps
+    # https://community.plot.ly/t/allowing-users-to-download-csv-on-click/5550/9
+    # XXX possibly remove this
+    df["month"] = df["month"].dt.strftime("%Y-%m-%d")
+    df["result_category"] = df["result_category"].replace(settings.ERROR_CODES)
+    return df.to_dict("records")
--- dashapp/apps/test_counts.py
+++ dashapp/apps/test_counts.py
@@ -59,38 +59,3 @@ def update_counts(page_state):
             )
         )
     return {"data": traces}
-
-
-@app.callback(Output("counts-table", "data"), [Input("page-state", "children")])
-def update_counts_table(page_state):
-    page_state = get_state(page_state)
-    if page_state.get("page_id") != settings.COUNTS_CHART_ID:
-        return {}
-
-    numerators = page_state.get("numerators", [])
-    denominators = page_state.get("denominators", [])
-    result_filter = page_state.get("result_filter", [])
-    groupby = page_state.get("groupby", None)
-    practice_filter_entity = page_state.get("practice_filter_entity", None)
-    entity_ids_for_practice_filter = page_state.get(
-        "entity_ids_for_practice_filter", []
-    )
-
-    if groupby == "practice":
-        col_name = "practice_id"
-    else:
-        col_name = groupby
-    # XXX should page with python here
-    # See https://dash.plot.ly/datatable/callbacks
-    df = get_count_data(
-        numerators=numerators,
-        denominators=denominators,
-        by=None,
-        practice_filter_entity=practice_filter_entity,
-        entity_ids_for_practice_filter=entity_ids_for_practice_filter,
-        result_filter=result_filter,
-    )
-    # XXX possibly remove this
-    df["month"] = df["month"].dt.strftime("%Y-%m-%d")
-    df["result_category"] = df["result_category"].replace(settings.ERROR_CODES)
-    return df.to_dict("records")
